0ad80e5ce8f4e0fc88b546480368c6fa1cdc152f
rolandoQuintana <danielquintanar3@gmail.com>
2025-03-31 21:14:31 -0600

    added stuff

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,3 +1,8 @@
+// This sets static prerendering
 export const prerender = true;
+
+// This disables server-side rendering completely
 export const ssr = false;
+
+// No trailing slash in routes
 export const trailingSlash = "never";
