1b4a8725d3b511c056a3ad8bd7d19702d0e4c5ba
rolandoQuintana <danielquintanar3@gmail.com>
2025-03-31 20:26:36 -0600

    Update deployment configuration and combine workflow jobs

--- svelte.config.js
+++ svelte.config.js
@@ -17,7 +17,10 @@ const config = {
 			fallback: 'index.html',
 			precompress: false,
 			strict: true
-		})
+		}),
+		paths: {
+			base: process.env.NODE_ENV === 'production' ? '/rolandoquintana_pages_svelte' : ''
+		}
 	}
 };
 
