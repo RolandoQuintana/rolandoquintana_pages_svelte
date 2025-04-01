5bce4459c331d5d209249ef88c2613c62fcbca5e
rolandoQuintana <danielquintanar3@gmail.com>
2025-03-31 20:52:55 -0600

    Fix base path configuration for GitHub Pages

--- svelte.config.js
+++ svelte.config.js
@@ -19,7 +19,7 @@ const config = {
 			strict: true
 		}),
 		paths: {
-			base: process.env.NODE_ENV === 'production' ? '/rolandoquintana_pages_svelte' : ''
+			base: '/rolandoquintana_pages_svelte'
 		}
 	}
 };
--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import { defineConfig } from 'vite';
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [sveltekit()],
-	base: '/rolandoquintana_pages_svelte/',
+	base: '/rolandoquintana_pages_svelte/'
 });
