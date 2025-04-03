4451d39ad4fd857526ffd514ac5938f59832bb0e
rolandoQuintana <danielquintanar3@gmail.com>
2025-04-03 11:12:42 -0600

    Update GitHub Pages configuration

--- svelte.config.js
+++ svelte.config.js
@@ -1,16 +1,11 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
-const dev = process.env.NODE_ENV !== 'production';
-
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://svelte.dev/docs/kit/integrations
-	// for more information about preprocessors
 	preprocess: vitePreprocess(),
 
 	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapters
 		adapter: adapter({
 			pages: 'build',
 			assets: 'build',
@@ -19,10 +14,7 @@ const config = {
 			strict: true
 		}),
 		paths: {
-			base: ''
-		},
-		prerender: {
-			handleMissingId: 'ignore'
+			base: '/rolandoquintana_pages_svelte'
 		}
 	}
 };
